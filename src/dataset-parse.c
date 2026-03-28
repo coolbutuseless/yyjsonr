1e4aeaf86d9e7f4c5892eb722d941db62da87422
mike <mikefc@coolbutuseless.com>
2026-03-28 10:24:31 +1000

    parse colspec dataTypes

--- src/dataset-parse.c
+++ src/dataset-parse.c
@@ -20,6 +20,53 @@
 #include "utils.h"
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Dataset NDJSON types
+// See cdisc spec: 
+//    https://github.com/cdisc-org/DataExchange-DatasetJson/blob/master/doc/dataset-json1-1.md
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// dataType (logical) 	JSON data type 	targetDataType 	Comment
+// -------------------  --------------  --------------  ------------
+//              string 	        string 		
+//             integer 	       integer 		
+//             decimal 	        string 	      decimal 	decimal is exchanged as a 
+//                                                      string and uses "." as the decimal separator
+//               float        	number 		
+//              double        	number 		
+//             boolean        	boolean 		
+//            datetime         	string 		              ISO 8601 datetime as a string
+//                date 	        string 	              	ISO 8601 date as a string
+//                time 	        string 		              ISO 8601 time as a string
+//            datetime         	string 	      integer 	ISO 8601 datetime as an integer (use case: ADaM)
+//                date        	string      	integer 	ISO 8601 date as an integer (use case: ADaM)
+//                time 	        string 	      integer 	ISO 8601 time as an integer (use case: ADaM)
+//                 URI 	        string 		
+
+#define DS_STRING   0
+#define DS_INTEGER  1
+#define DS_DECIMAL  2
+#define DS_FLOAT    3
+#define DS_DOUBLE   4
+#define DS_BOOLEAN  5
+#define DS_DATE     7
+#define DS_TIME     8
+#define DS_URI      9
+
+#define NDTYPES 10
+
+const char *ref_dtype_str[NDTYPES] = {
+  "string",
+  "integer",
+  "decimal",
+  "float",
+  "double", 
+  "boolean",
+  "datetime", 
+  "date", 
+  "time", 
+  "uri"
+};  
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // colspec example
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //         itemOID    name                     label dataType length keySequence
@@ -41,14 +88,10 @@ SEXP parse_dataset_ndjson_str_as_df_(SEXP str_, SEXP colspec_, SEXP nskip_, SEXP
   // Parse the colspec for some sanity
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   if (!Rf_isDataFrame(colspec_)) Rf_error("parse_dataset_ndjson_str_as_df_(): 'colspec' must be a data.frame");
-  int colspec_rows = Rf_nrows(colspec_);
   int nm_idx = df_col_idx(colspec_, "name");
   if (nm_idx < 0) Rf_error("parse_dataset_ndjson_str_as_df_(): 'name' not found in colspec");
   int dt_idx = df_col_idx(colspec_, "dataType");
   if (dt_idx < 0) Rf_error("parse_dataset_ndjson_str_as_df_(): 'dataType' not found in colspec");
-  
-  Rprintf("Colspec nrows %i, nm: %i, dataType: %i\n", colspec_rows, nm_idx, dt_idx);
-  
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Iterate over the file.  For each line
@@ -120,6 +163,38 @@ SEXP parse_dataset_ndjson_str_as_df_(SEXP str_, SEXP colspec_, SEXP nskip_, SEXP
   }
   Rprintf("nrows of data: %i\n", (int)nrows);
   
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Prepare data.frame
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  SEXP dtypes_ = VECTOR_ELT(colspec_, dt_idx);
+  int colspec_nrows = (int)Rf_length(dtypes_);
+  
+  int *dtype   = malloc(colspec_nrows * sizeof(int));
+  if (dtype == NULL) Rf_error("parse_dataset_ndjson_str_as_df_(): Failed to alloc 'dtypes'");
+  
+  Rprintf("dtypes: %i %s\n", colspec_nrows, Rf_type2char(TYPEOF(dtypes_)));
+  for (int i = 0; i < colspec_nrows; i++) {
+    const char *dtype_str = CHAR(STRING_ELT(dtypes_, i));
+    
+    bool found = false;
+    for (int d = 0; d < NDTYPES; d++) {
+      if (strcmp(dtype_str, ref_dtype_str[d]) == 0) {
+        dtype[i] = d;
+        found = true;
+        break;
+      }
+    }
+    
+    if (!found) {
+      free(dtype);
+      Rf_error("parse_dataset_ndjson_str_as_df_(): Unknown dataType in colspec: '%s'", dtype_str);
+    }
+    
+  }
+  
+  for (int i = 0; i < colspec_nrows; i++) {
+    Rprintf("%i %i %s\n", i, dtype[i], ref_dtype_str[dtype[i]]);
+  }
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Parse data row-by-row
@@ -130,6 +205,7 @@ SEXP parse_dataset_ndjson_str_as_df_(SEXP str_, SEXP colspec_, SEXP nskip_, SEXP
     state->doc = yyjson_read_opts(str, str_size, opt.yyjson_read_flag, NULL, &err);
     size_t pos = yyjson_doc_get_read_size(state->doc);
     if (state->doc == NULL) {
+      free(dtype);
       error_and_destroy_state(state, "Couldn't parse Dataset-NDJSON on line %i\n", i + 1);
     }
     
@@ -138,12 +214,21 @@ SEXP parse_dataset_ndjson_str_as_df_(SEXP str_, SEXP colspec_, SEXP nskip_, SEXP
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     yyjson_val *row = yyjson_doc_get_root(state->doc);
     if (yyjson_get_type(row) != YYJSON_TYPE_ARR) {
+      free(dtype);
       error_and_destroy_state(state, "Couldn't parse Dataset-NDJSON row %i - JSON type = '%s' not 'array'\n", 
                               i + 1, yyjson_get_type_desc(row));
     }
     
     Rprintf("[%i] %s\n", i, yyjson_get_type_desc(row));
 
+    
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Parse items out of row
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    int nitems = yyjson_get_len(row);
+    Rprintf("Row items: %i/%i\n", nitems, colspec_nrows);
+    
+    
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Advance string 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -153,7 +238,7 @@ SEXP parse_dataset_ndjson_str_as_df_(SEXP str_, SEXP colspec_, SEXP nskip_, SEXP
   }
   
   
-  
+  free(dtype);
   destroy_state(state);
   UNPROTECT(nprotect);
   return R_NilValue;
